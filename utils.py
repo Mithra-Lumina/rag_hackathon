dbb795fe9238704ac3d77a919992236d2343dc3a
Behrad <behrad.hemati@gmail.com>
2025-06-01 22:47:33 +0200

    add agents

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 def markdown_cleanup(md_text):
     md_text = re.sub('\n+', '\n', md_text)
@@ -37,3 +41,51 @@ def os_temp(thing):
     return {
         "_source": thing
     }
+
+def plot_temps(start_time, end_time):
+    df = pd.read_json('data/climate_edu/global_warming.jsonl', lines=True, dtype={'time': str})
+    df = df.round(2)
+    filtered_df = df[(df['time'] >= start_time) & (df['time'] <= end_time)] 
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    #  Create the line plot
+    sns.lineplot(x='time', y='station', data=filtered_df, ax=ax)
+
+    # Set labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Average Temperature')
+    ax.set_title('Average Global Temperature Over Time')
+
+    # Handle x-axis ticks
+    ticks = ax.get_xticks()
+    tick_positions = np.linspace(ticks[0], ticks[-1], 10, dtype=int) if len(ticks) > 10 else ticks
+    ax.set_xticks(tick_positions)
+    ax.tick_params(axis='x', rotation=45)
+
+    # Adjust layout2000.01'
+    plt.tight_layout()
+    return fig, ax
+
+def plot_co2(start_time, end_time):
+    df = pd.read_json('data/climate_edu/co2.jsonl', lines=True, dtype={'time': str})
+    df = df.round(2)
+    filtered_df = df[(df['time'] >= start_time) & (df['time'] <= end_time)] 
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    # Create the line plot
+    sns.lineplot(x='time', y='cycle', data=filtered_df, ax=ax)
+
+    # Set labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('CO2 Emissions (ppm)')
+    ax.set_title('CO2 Emissions Over Time')
+
+    # Handle x-axis ticks
+    ticks = ax.get_xticks()
+    tick_positions = np.linspace(ticks[0], ticks[-1], 10, dtype=int) if len(ticks) > 10 else ticks
+    ax.set_xticks(tick_positions)
+    ax.tick_params(axis='x', rotation=45)
+
+    # Adjust layout
+    plt.tight_layout()
+    return fig, ax

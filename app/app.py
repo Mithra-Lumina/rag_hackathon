7187a80f83b6503f410958ee592bd89a39ba2d12
Alireza Rezaei <84126554+RezaeiAlireza@users.noreply.github.com>
2025-06-02 13:13:27 +0200

    Agentic logic for generating CO2 and temperature plot added

--- app/app.py
+++ app/app.py
@@ -6,9 +6,10 @@ from langchain_chroma import Chroma
 from langchain_core.callbacks.base import BaseCallbackHandler
 from langchain_core.messages import (
     HumanMessage,
-    SystemMessage,
-    AIMessage
+    SystemMessage
 )
+import fitz
+import utils
 
 class StreamingGradioCallbackHandler(BaseCallbackHandler):
     def __init__(self, chatbot_messages_list):
@@ -29,6 +30,7 @@ def load_environment():
         "IBM_URL": os.getenv("URL"),
         "PROJECT_ID": os.getenv("PROJECT_ID")
     }
+config = load_environment()
 
 def get_embedder(api_key, url, project_id):
     return WatsonxEmbeddings(
@@ -41,13 +43,18 @@ def get_embedder(api_key, url, project_id):
 def initialize_vector_store(embedder):
     return Chroma(
         collection_name="climate_edu",
-        persist_directory="data/chroma_db",
+        persist_directory="app/data/chroma_db",
         embedding_function=embedder
     )
-
-def retrieve_relevant_docs(vector_store, query, k=2):
-    docs = vector_store.similarity_search(query, k=k)
-    return [doc.page_content for doc in docs]
+embedder = get_embedder(config["API_KEY"], config["IBM_URL"], config["PROJECT_ID"])
+vector_store = initialize_vector_store(embedder)
+def retrieve_relevant_docs(query, k=5):
+    docs = vector_store.similarity_search_with_relevance_scores(query, k=k, score_threshold=0.50)
+    page_content = [doc[0].page_content for doc in docs]
+    page_number = [doc[0].metadata['page'] for doc in docs]
+    file_name = [doc[0].metadata['file_name'] for doc in docs]
+    gallery_images(file_name, page_number)
+    return page_content, file_name, page_number
 
 
 def format_context(docs):
@@ -93,7 +100,7 @@ def get_llm(api_key, url, project_id):
         url=url,
         apikey=api_key,
         project_id=project_id,
-        params={"max_new_tokens": 1024, "decoding_method": "greedy"},
+        params={"max_new_tokens": 100, "decoding_method": "greedy"},
     )
 
 def user_fn(user_message, chat_history):
@@ -101,19 +108,17 @@ def user_fn(user_message, chat_history):
     chat_history.append({"role": "user", "content": user_message})
     return chat_history, gr.Textbox(value=user_message, interactive=True)
 
-config = load_environment()
-embedder = get_embedder(config["API_KEY"], config["IBM_URL"], config["PROJECT_ID"])
-vector_store = initialize_vector_store(embedder)
 
+llm = get_llm(config["API_KEY"], config["IBM_URL"], config["PROJECT_ID"])
+local_custom_agent = utils.LangGraphApp()
+local_custom_agent.set_up(llm)
 def chatbot_interface(user_message, chat_history):
-
+    response = local_custom_agent.query(user_message)
     chat_history = chat_history or []
     chat_history.append({"role": "assistant", "content": ""})
 
-    llm = get_llm(config["API_KEY"], config["IBM_URL"], config["PROJECT_ID"])
-
-    docs = retrieve_relevant_docs(vector_store, user_message)
-    context = format_context(docs)
+    content, name, page_number = retrieve_relevant_docs(user_message)
+    context = "\n".join([f"Document {i + 1}:\n{doc}" for i, doc in enumerate(content)])
     history = format_chat_history(chat_history)
     prompt = build_prompt(context, user_message, history)
 
@@ -121,11 +126,51 @@ def chatbot_interface(user_message, chat_history):
     for chunk in streamer:
         chat_history[-1]["content"] += chunk.content
         yield chat_history, gr.Textbox(value="", interactive=True)
-def gallery_images():
+
+def export_page_fitz(file_name, page_number):
+    source_path = os.path.join("app/data/climate_edu", file_name + ".pdf")
+    doc = fitz.open(source_path)
+
+    if page_number < 1 or page_number > len(doc):
+        print("Invalid page number")
+        return
+
+    page = doc.load_page(page_number)
+    pix = page.get_pixmap(dpi=200)
+    os.makedirs("app/output", exist_ok=True)
+    output_path = f"app/output/test{page_number}-{file_name}.png"
+    pix.save(output_path)
+    print(f"Saved: {output_path}")
+
+def gallery_images(file_names, page_numbers):
+    # Clean old images
+    if len(file_names) >= 1:
+        if os.path.exists("app/output"):
+            for f in os.listdir("app/output"):
+                if f.endswith(".png"):
+                    os.remove(os.path.join("app/output", f))
+
+    for file_name, page_number in zip(file_names, page_numbers):
+        export_page_fitz(file_name, page_number)
+        print(file_name, page_number)
+
+    
+def show_gallery():
     images = [
-                "app/climate-change-concept-illustration_114360-27216.avif",
-                "app/TY6SwtikwK9QzrrmuZcfoj.jpg",
-            ]
+        os.path.join("app/output", img)
+        for img in os.listdir("app/output")
+        if img.endswith(".png")
+    ]
+
+    return images
+
+def show_plot():
+    images = [
+        os.path.join("app/plot_output", img)
+        for img in os.listdir("app/plot_output")
+        if img.endswith(".png")
+    ]
+
     return images
 
 with gr.Blocks(css_paths="./app/styles.css", theme=gr.Theme.from_hub("JohnSmith9982/small_and_pretty")) as demo:
@@ -144,32 +189,61 @@ with gr.Blocks(css_paths="./app/styles.css", theme=gr.Theme.from_hub("JohnSmith9
             with gr.Row():
                 with gr.Group(elem_id="doc-res"):
                     gr.Markdown(elem_id="header-main", value="# 📈 Visuallization and Resources 📄")
-                    gr.TextArea(autoscroll= False, show_copy_button=True,show_label=False, value= "This is a climate education system that uses RAG pipeline to help you understand more about climate change, its impacts, and how to mitigate it. Using this AI-dirven system you can ask how to tackle different environmental problems and use it for self-education instead of surfing the internet for sparse knowledge in this area. It has access to a broad range of materials based on truth with statistical information to guide you.")
+                    gallerya = gr.Gallery(
+                    value=['app/data/placeholder_img/logo.png'],
+                    show_label=False,
+                    elem_id="gallerya",
+                    object_fit="contain",
+                    height="auto",
+                    interactive=True
+                    )            
             with gr.Row(elem_id="plot-area"):
                 gallery = gr.Gallery(
-                    show_label=False, elem_id="gallery",
-                    object_fit="contain", height="auto", selected_index=1, value=gallery_images
+                    value=['app/data/placeholder_img/climate_change.png'],
+                    show_label=False,
+                    elem_id="gallery",
+                    object_fit="contain",
+                    height="auto",
+                    interactive=True
                 )
-
                 demo.load(None, None, None, js="() => { document.body.classList.remove('dark'); }")
     send_btn.click(
         fn=user_fn,
         inputs=[user_input, chatbot],
         outputs=[chatbot, user_input]
     ).then(
+        fn=retrieve_relevant_docs,
+        inputs=[user_input],
+        outputs=[None, None, None]
+    ).then(
+        fn=show_gallery,
+        outputs=[gallery]
+    ).then(
         fn= chatbot_interface,
-        inputs=[user_input, chatbot],
+        inputs=[gr.State(value=""), chatbot],
         outputs=[chatbot, user_input]
-        )
+        ).then(
+        fn=show_plot,
+        outputs=[gallerya]
+    )
     user_input.submit(
         fn=user_fn,
         inputs=[user_input, chatbot],
         outputs=[chatbot, user_input]
     ).then(
+        fn=retrieve_relevant_docs,
+        inputs=[user_input],
+        outputs=[None, None, None]
+    ).then(
+        fn=show_gallery,
+        outputs=[gallery]
+    ).then(
         fn= chatbot_interface,
-        inputs=[user_input, chatbot],
+        inputs=[gr.State(value=""), chatbot],
         outputs=[chatbot, user_input]
-        )
-
+        ).then(
+        fn=show_plot,
+        outputs=[gallerya]
+    )
 if __name__ == "__main__":
     demo.launch()
---
+++ app/utils.py
@@ -0,0 +1,163 @@
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from typing import Literal
+from langchain_core.tools import tool
+from matplotlib import figure
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+from langgraph.prebuilt import ToolNode
+from pydantic import BaseModel, Field
+
+def markdown_cleanup(md_text):
+    md_text = re.sub('\n+', '\n', md_text)
+    md_text = re.sub(r'^\n+', '', md_text)
+    md_text = re.sub('\n', 'PARAGRAPH_BREAK', md_text)
+    md_text = re.sub(r'\s+', ' ', md_text)
+    md_text = md_text.replace('PARAGRAPH_BREAK', '\n')
+    return md_text
+
+def boolean_filter(documents):
+    return {
+    "bool": {
+        "should": [
+        {
+            "terms": {
+            "title": documents
+            }
+        },
+        {
+            "terms": {
+            "title.keyword": documents
+            }
+        }
+        ]
+    }
+    }
+
+def opensearch_format(results):
+    return {
+        "hits": {
+            "hits": results
+        }
+    }
+
+def os_temp(thing):
+    return {
+        "_source": thing
+    }
+
+def plot_temps(start_time, end_time, out_path=None):
+    df = pd.read_json('app/data/climate_edu/global_warming.jsonl', lines=True, dtype={'time': str})
+    df = df.round(2)
+    filtered_df = df[(df['time'] >= start_time) & (df['time'] <= end_time)] 
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    #  Create the line plot
+    sns.lineplot(x='time', y='station', data=filtered_df, ax=ax)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Average Temperature')
+    ax.set_title('Average Global Temperature Over Time')
+
+    ticks = ax.get_xticks()
+    tick_positions = np.linspace(ticks[0], ticks[-1], 10, dtype=int) if len(ticks) > 10 else ticks
+    ax.set_xticks(tick_positions)
+    ax.tick_params(axis='x', rotation=45)
+
+    plt.tight_layout()
+
+    # If the caller passed an output path, save the figure:
+    if out_path is not None:
+        fig.savefig(out_path, dpi=300) 
+    return fig, ax
+
+
+def plot_co2(start_time, end_time, out_path=None):
+    df = pd.read_json('app/data/climate_edu/co2.jsonl', lines=True, dtype={'time': str})
+    df = df.round(2)
+    filtered_df = df[(df['time'] >= start_time) & (df['time'] <= end_time)] 
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    sns.lineplot(x='time', y='cycle', data=filtered_df, ax=ax)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('CO2 Emissions (ppm)')
+    ax.set_title('CO2 Emissions Over Time')
+
+    ticks = ax.get_xticks()
+    tick_positions = np.linspace(ticks[0], ticks[-1], 10, dtype=int) if len(ticks) > 10 else ticks
+    ax.set_xticks(tick_positions)
+    ax.tick_params(axis='x', rotation=45)
+    
+    plt.tight_layout()
+
+    if out_path is not None:
+        fig.savefig(out_path, dpi=300)
+    return fig, ax
+
+class TempGraphInputSchema(BaseModel):
+    """Start and end dates for temperature graph generation."""
+    start_date: str = Field(description="Start date in the format YYYY.MM")
+    end_date: str = Field(description="End date in the format YYYY.MM")
+
+@tool("create_temperature_graph", args_schema=TempGraphInputSchema)
+def create_temperature_graph(start_date: str, end_date: str) -> figure:
+    return plot_temps(start_date, end_date, out_path="app/plot_output/temperature_graph.png")
+
+class Co2GraphInputSchema(BaseModel):
+    """Start and end dates for temperature graph generation."""
+    start_date: str = Field(description="Start date in the format YYYY.MM.DD")
+    end_date: str = Field(description="End date in the format YYYY.MM.DD")
+
+@tool("create_co2_graph", args_schema=Co2GraphInputSchema)
+def create_co2_graph(start_date: str, end_date: str) -> figure:
+    return plot_co2(start_date, end_date, out_path="app/plot_output/co2_graph.png")
+
+
+def router(
+    state: list[BaseMessage],
+) -> Literal["create_temperature_graph","create_co2_graph", "__end__"]:
+    """Creates tempreture graph."""
+    # Get the tool_calls from the last message in the conversation history.
+    tool_calls = state[-1].tool_calls
+
+    # If there are any tool_calls
+    if tool_calls:
+        # Check the function name in the first tool call
+        function_name = tool_calls[0].get("name")
+        if function_name == "create_temperature_graph":
+            return "create_temperature_graph"
+        else:
+            return "create_co2_graph"
+    else:
+        # End the conversation flow.
+        return "__end__"
+    
+class LangGraphApp:
+    # The set_up method is used to define application initialization logic
+    def set_up(self, ibm_chat) -> None:
+        model = ibm_chat
+        builder = MessageGraph()
+
+        model_with_tools = model.bind_tools([create_temperature_graph, create_co2_graph])
+        builder.add_node("tools", model_with_tools)
+
+        tool_node = ToolNode([create_temperature_graph, create_co2_graph])
+        builder.add_node("create_temperature_graph", tool_node)
+        builder.add_node("create_co2_graph", tool_node)
+        builder.add_edge("create_temperature_graph", END)
+        builder.add_edge("create_co2_graph", END)
+
+        builder.set_entry_point("tools")
+        builder.add_conditional_edges("tools", router)
+        self.app = builder.compile()
+
+    # The query method will be used to send inputs to the agent
+    def query(self, input: str):
+        """Query the application."""
+        chat_history = (self.app.invoke(HumanMessage(input)))
+        return chat_history
